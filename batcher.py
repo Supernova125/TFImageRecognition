e128e6032706c1c005091976b2d32b9fc7f97444
Supernova125 <79452660+Supernova125@users.noreply.github.com>
2024-01-05 21:21:26 +0100

    batcher.py now balances imgs per game

--- batcher.py
+++ batcher.py
@@ -1,4 +1,4 @@
-import pathlib, os, random
+import pathlib, random
 from shutil import rmtree, copy
 from toml import load as tload
 
@@ -7,9 +7,9 @@ config = tload('config.toml')
 print("Starting batcher...")
 print("Clearing folder...")
 # empty whole folder
-if os.path.exists(config["batcher"]["batch_folder_name"]):
+if pathlib.Path(config["batcher"]["batch_folder_name"]).exists():
     rmtree(config["batcher"]["batch_folder_name"])
-    os.makedirs(config["batcher"]["batch_folder_name"])
+pathlib.Path(config["batcher"]["batch_folder_name"]).mkdir(parents=True, exist_ok=True)
 
 print("Finding images...")
 data_dir = pathlib.Path('game_images').with_suffix("")
@@ -20,24 +20,39 @@ tar_image_count = config["batcher"]["tar_image_count"]
 
 image_chance = tar_image_count / image_count
 
-print("\nChance: " + str(image_chance))
+games = list(x.name for x in pathlib.Path("game_images").iterdir())
+imgs_per_game = int(tar_image_count / len(games))
+
+print("\nGlobal Chance: " + str(image_chance))
 print("Batch size: " + str(tar_image_count))
-print("All count: " + str(image_count))
+print("Per Game: " + str(imgs_per_game))
+print("All Imgs: " + str(image_count))
 print("Starting to copy...")
 
 # Iterate over each image in the data_dir and randomly decide whether to copy it
 copiedfiles = 0
-while copiedfiles < tar_image_count:
-    for image_path in data_dir.glob('*/*.jpg'):
-        if random.random() < image_chance:
-            if copiedfiles >= tar_image_count:
+for game in games:
+    print("Copying to " + str(config["batcher"]["batch_folder_name"]) + "/" + str(game))
+    imgs = list(x for x in pathlib.Path("./game_images/"+game).iterdir() if x.name.endswith(".jpg"))
+    print("Found " + str(len(imgs)) + " images for " + str(game))
+    if len(imgs) == 0:
+        continue
+    local_chance = imgs_per_game / len(imgs)
+    #print("Chance: " + str(local_chance))
+    local_copied = 0
+    while local_copied < imgs_per_game and local_copied < len(imgs): # prevents too small folders having multiple same imgs
+        for image_path in imgs:
+            if local_copied >= imgs_per_game:
                 break
-            destination_dir = pathlib.Path(config["batcher"]["batch_folder_name"]) / image_path.parent.relative_to(data_dir)
-            destination_dir.mkdir(parents=True, exist_ok=True)
-            destination_path = destination_dir / image_path.name
-            # copy
-            copy(image_path, destination_path)
-            copiedfiles += 1
+            if random.random() < local_chance:
+                destination_dir = pathlib.Path(config["batcher"]["batch_folder_name"]) / image_path.parent.relative_to(data_dir)
+                destination_dir.mkdir(parents=True, exist_ok=True)
+                destination_path = destination_dir / image_path.name
+                # copy
+                copy(image_path, destination_path)
+                copiedfiles += 1
+                local_copied += 1
+    print("Copied " + str(local_copied) + " images to " + str(config["batcher"]["batch_folder_name"]) + "/" + str(game))
 
 print("Done!")
 print("Copied " + str(copiedfiles) + " images to " + str(config["batcher"]["batch_folder_name"]))
